d67b0a1f6695fe84d3917cbb5309e1d33245c381
__HMB <82461527+HMB6000@users.noreply.github.com>
2026-03-16 14:13:23 +0200

    Add project files.

---
+++ Jarv1s/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace Jarv1s
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ASCII ascii = new ASCII(); //an instance of the ASCII class 
+            ascii.DisplayASCIIART(); //to call the DisplayASCIIART method
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Hello, User!");
+                Console.ResetColor();
+
+                ResponseSys response = new ResponseSys();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("User: ");
+                string userInput = Console.ReadLine().ToLower();
+                string printResponse = response.UserResponse(userInput);
+                if (userInput == "exit")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Exiting Jarv1s. Goodbye!");
+                    Console.ResetColor();
+                    break;
+                }
+                Console.WriteLine(printResponse);
+                Console.ResetColor();
+            }
+            
+
+
+
+        }
+    }
+}
---
+++ Jarv1s/ResponseSys.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jarv1s
+{
+    public class ResponseSys
+    {
+        private Dictionary<string, string> responses = new Dictionary<string, string>
+        {
+            { "hello", "Hello! How can I assist you today?" },
+            { "hi", "Hello! How can I assist you today?" },
+            { "hey", "Hello! How can I assist you today?" },
+            { "purpose", "To provide educational informaion about cybersecurity." },
+            { "help", "Use keywords such as, purpose, can, help, exit." },
+            { "what can", "Type help to see my available capabilities." },
+            { "threat", "There are different types of threats such ad: DDos, Spoofing, Phising,..." },
+            { "thanks", "You're welcome! If you have any more questions, feel free to ask." }
+        };
+
+        public string UserResponse(string userInput)
+        {
+            userInput = userInput.ToLower();
+            foreach (var response in responses)
+            {
+                if (userInput.Contains(response.Key) || userInput.ToLower() == "exit")
+                {
+                    return response.Value;
+                }
+            }
+            return "I'm sorry, I don't understand. Can you please rephrase?";
+        }
+    }
+}
+
