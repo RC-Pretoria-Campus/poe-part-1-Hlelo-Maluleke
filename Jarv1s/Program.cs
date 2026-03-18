390bf9b11875955527ca8753114c88e60846f4ab
__HMB <82461527+HMB6000@users.noreply.github.com>
2026-03-18 17:51:12 +0200

    added more bot responses and clean up the code.

--- Jarv1s/Program.cs
+++ Jarv1s/Program.cs
@@ -1,40 +1,47 @@
-﻿using System;
+﻿using Jarv1s;
+using System;
 using System.Reflection.Metadata;
 
-namespace Jarv1s
+ASCII ascii = new ASCII(); //an instance of the ASCII class 
+ascii.DisplayASCIIART(); //to call the DisplayASCIIART method
+
+UserIntro userIntro = new UserIntro(); //an instance of the UserIntro class
+userIntro.GetUserName(); //to call the GetUserName method
+
+string userName = null;
+while (string.IsNullOrWhiteSpace(userName))
 {
-    class Program
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Hello. Please enter your name: ");
+    Console.ResetColor();
+    userName = Console.ReadLine().ToUpper();
+
+    if (userName == null || string.IsNullOrWhiteSpace(userName))
     {
-        static void Main(string[] args)
-        {
-            ASCII ascii = new ASCII(); //an instance of the ASCII class 
-            ascii.DisplayASCIIART(); //to call the DisplayASCIIART method
-
-            while (true)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Hello, User!");
-                Console.ResetColor();
-
-                ResponseSys response = new ResponseSys();
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write("User: ");
-                string userInput = Console.ReadLine().ToLower();
-                string printResponse = response.UserResponse(userInput);
-                if (userInput == "exit")
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Exiting Jarv1s. Goodbye!");
-                    Console.ResetColor();
-                    break;
-                }
-                Console.WriteLine(printResponse);
-                Console.ResetColor();
-            }
-            
-
-
-
-        }
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Invalid input. Please enter a valid name.");
+        Console.ResetColor();
     }
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"Welcome to Jarv1s, {userName}! How can I assist you today? Type 'help' for options.");
+Console.ResetColor();
+
+while (true)
+{
+    ResponseSys response = new ResponseSys();
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.Write($"{userName}: ");
+    string userInput = Console.ReadLine().ToLower();
+    string printResponse = response.UserResponse(userInput);
+    if (userInput == "exit")
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Exiting Jarv1s. Goodbye!");
+        Console.ResetColor();
+        break;
+    }
+    Console.WriteLine(printResponse);
+    Console.ResetColor();
 }
